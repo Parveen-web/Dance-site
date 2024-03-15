419e8613b11a107ecb9d7a44850de9804ba14a52
Parveen <parveent469@gmail.com>
2024-03-15 23:05:18 +0530

    initial commit

---
+++ static/index.js
@@ -0,0 +1,12 @@
+doctype html
+
+html
+    head
+        style
+            include style.css
+
+    body
+        h2 My site
+        p welcom to my dance site
+        Script
+            include index.js        
